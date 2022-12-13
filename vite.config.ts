7226bf9f9f7a06ebb819b217b585435f055a8bb8
Shindou Mihou <git@mihou.pw>
2022-12-14 01:56:27 +0800

    feat: adds offline mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 const config: UserConfig = {
-	plugins: [sveltekit()]
+    plugins: [sveltekit(), VitePWA({ registerType: 'autoUpdate' })]
 };
 
 export default config;
